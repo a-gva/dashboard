29e7263729ea62be04d7948180ea0fd1f31c83ae
Alexy Viegas <alexyviegas.dev@gmail.com>
2023-02-05 17:21:12 -0300

    chore: fix build entry point reference

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
-  build: {
-    outDir: '../dist',
-  },
+  // root: 'src',
+  // build: {
+  //   outDir: '../dist',
+  // },
 });
