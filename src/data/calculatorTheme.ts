307cf6749419cb1ed74ef071b7423587127b8a80
Alexy Viegas <alexyviegas.dev@gmail.com>
2023-02-07 21:33:11 -0300

    styling: add calculator themes

---
+++ src/data/calculatorTheme.ts
@@ -0,0 +1,44 @@
+export const calculatorTheme = {
+  dark: {
+    background: '#18181C',
+    numbers: {
+      background: '#2E2E38',
+      primaryColor: '#FFFFFF',
+      secondaryColor: '#B3B3B3',
+    },
+    operators: {
+      background: '#4B5EFC',
+      color: '#FFFFFF',
+    },
+    specialOperators: {
+      background: '#2E2E38',
+      color: '#FFFFFF',
+    },
+    themeButton: {
+      background: '#2E2E38',
+      buttonBackground: '#4E505F',
+      icon: '#4150C0',
+    },
+  },
+  light: {
+    background: '#F1F2F3',
+    numbers: {
+      background: '#FFFFFF',
+      primaryColor: '#000000',
+      secondaryColor: '#919192',
+    },
+    operators: {
+      background: '#4B5EFC',
+      color: '#FFFFFF',
+    },
+    specialOperators: {
+      background: '#D2D3DA',
+      color: '#000000',
+    },
+    themeButton: {
+      background: '#FFFFFF',
+      buttonBackground: '#D2D3DA',
+      icon: '#808EFD',
+    },
+  },
+};
